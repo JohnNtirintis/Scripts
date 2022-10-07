56416c27166ab937d1354a902b2137c20490272b
John Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2022-10-07 14:30:26 +0300

    Floyd's Triangle in Kotlin

---
+++ Floyd Triangle.kt
@@ -0,0 +1,12 @@
+fun floydTriangle(){
+    var count = 1
+    var limit = 5
+    for(i in 1.. limit){
+        repeat(i){
+            print("${count++} ")
+        }
+        println()
+    }
+
+
+}
