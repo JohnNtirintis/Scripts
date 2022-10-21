beb39b04e4878976901cd4563986777c7e22ff08
John Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2022-10-21 13:51:27 +0300

    Cleaned the Code. Deleted a few spaces and empty lines here and there to make it easier readable.

--- C/Triangle.c
+++ C/Triangle.c
@@ -2,26 +2,22 @@
 
 int main(void)
 {
-    int height;
-   printf("Enter the height of the pyramid: ");
-    scanf ("%d",&height);
-
-        for(int i = 0; i < height; i++){
-            for(int j = 0; j < height; j++){
-                if(j + i< height - 1){
+    int height = 8;
+   
+        for(int i = 0; i < height; i++)
+        {
+            for(int j = 0; j < height; j++)
+            {
+                if(j + i< height - 1)
+                {
                     printf(" ");
                 }
                 else printf("#");
             }
-            for(int k = i; k > 0; k--){
-                    printf("#");
-                    //for(int w = i; w < height; w++){
-                      //  printf("  ");
-                    //}
+            for(int k = i; k > 0; k--)
+            {
+                printf("#");                  
             }
             printf("\n");
         }
-
-
-
 }
--- Kotlin/Floyd Triangle.kt
+++ Kotlin/Floyd Triangle.kt
@@ -1,12 +1,13 @@
-fun floydTriangle(){
+fun floydTriangle()
+{
     var count = 1
     var limit = 5
-    for(i in 1.. limit){
-        repeat(i){
+    for(i in 1.. limit)
+    {
+        repeat(i)
+        {
             print("${count++} ")
         }
         println()
     }
-
-
 }
