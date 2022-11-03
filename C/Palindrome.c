2c087889a9f6b706bc01f67e1bbdabb347703208
John Ntirintis <ioannis.panagiotis.diridis@gmail.com>
2022-11-03 23:13:50 +0200

    Add function for string arrays

--- C/Palindrome.c
+++ C/Palindrome.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int main(){
 
     int palindrome[] = {4,4,2,2,4,4};
+    char name_palindrome[] = "Anna";
     //Sizeof(arr) returns valye in bytes, so we need to divide the return value using the type of data we are using.
     //After we divide using the same type of data (equal bytes), we get the actual length of the array.
     int len = sizeof(palindrome) / sizeof(palindrome[0]);
@@ -14,6 +16,20 @@ int main(){
             return 1;
         }
     }
+    
+    //Code for String Arrays
+    len = strlen(name_palindrome);
+    for(int i = 0; i < len; i++){
+        //Must convert to all lower case or all capitcal because C compares chars in ASCII.
+        name_palindrome[i] = tolower(name_palindrome[i]);
+        if(name_palindrome[i] != name_palindrome[len - i - 1]){
+            return 1;
+        }
+    }
+   
+   
     return 0;
+
+
 
 }
